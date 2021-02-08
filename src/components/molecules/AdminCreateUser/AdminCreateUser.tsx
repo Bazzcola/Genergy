6340b39709bbf9cb6e514578ca7fb5b20dbe0b29
Delinski Roman <Wendigo91@inbox.ru>
2021-02-08 15:30:55 +0200

    little fixes

--- src/components/molecules/AdminCreateUser/AdminCreateUser.tsx
+++ src/components/molecules/AdminCreateUser/AdminCreateUser.tsx
@@ -4,188 +4,180 @@ import { Form, Input, Button, Checkbox, Radio,Switch } from 'antd';
 import './AdminCreateUser.scss';
 
 interface Form {
-    password:string,
-    username:string,
-    remember?:boolean;
+  password: string;
+  username: string;
+  remember?: boolean;
 }
 
 export const AdminCreateUser = () => {
-    const [form, setForm] = React.useState<Form>({password:'', username:''});
+  const [form, setForm] = React.useState<Form>({ password: '', username: '' });
 
-    const layout = {
-        labelCol: {
-          span: 8,
-        },
-        wrapperCol: {
-          span: 16,
-        },
-      };
-      const tailLayout = {
-        wrapperCol: {
-          offset: 8,
-          span: 16,
-        },
-      };
+  const layout = {
+    labelCol: {
+      span: 8
+    },
+    wrapperCol: {
+      span: 16
+    }
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16
+    }
+  };
 
-      const onFinish = (values:any) => {
-        setForm(values);
-      };
-      console.log('Success:', form);
+  const onFinish = (values: any) => {
+    setForm(values);
+  };
+  console.log('Success:', form);
 
-      const onFinishFailed = (errorInfo:any) => {
-        console.log('Failed:', errorInfo);
-      };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
 
-    return (
-        <div className="create-container">
-            <h1>Создать пользователя</h1>
-            <Form
-            {...layout}
-            name="basic"
-            initialValues={{
-            remember: false,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-        >
-            <Form.Item
-            label="Имя"
-            name="user_name"
+  return (
+    <div className="create-container">
+      <h1>Создать пользователя</h1>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: false
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+          <Form.Item
+            label="Username"
+            name="username"
             rules={[
-                {
+              {
                 required: true,
-                message: 'Please input username!',
-                },
+                message: 'Please input your username!'
+              }
             ]}
-            >
+          >
             <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!'
+              }
+            ]}
+          >
+            <Input type="password"/>
+          </Form.Item>
+            <Form.Item
+              label="Имя"
+              name="user_name"
+              rules={[
+                  {
+                  required: true,
+                  message: 'Please input username!',
+                  },
+              ]}
+            >
+              <Input />
             </Form.Item>
 
             <Form.Item name="user_sex" label="Пол" rules={[
-                {
-                required: true,
-                message: 'Please input gender type!',
-                },
-            ]}>
+                  {
+                  required: true,
+                  message: 'Please input gender type!',
+                  },
+              ]}
+            >
               <Radio.Group>
                 <Radio value="male">Male</Radio>
                 <Radio value="female">Female</Radio>
               </Radio.Group>
             </Form.Item>
             <Form.Item
-            label="IDNP"
-            name="user_idno"
-            rules={[
-                {
-                required: true,
-                message: 'Please input idno!',
-                },
-            ]}
-            >
-            <Input />
+              label="IDNP"
+              name="user_idno"
+              rules={[
+                  {
+                  required: true,
+                  message: 'Please input idno!',
+                  },
+              ]}
+              >
+              <Input />
             </Form.Item>
 
             <Form.Item
-            label="Телефон"
-            name="user_phone"
-            rules={[
-                {
-                required: true,
-                message: 'Please input phone!',
-                },
-            ]}
-            >
-            <Input />
+              label="Телефон"
+              name="user_phone"
+              rules={[
+                  {
+                  required: true,
+                  message: 'Please input phone!',
+                  },
+              ]}
+              >
+              <Input />
             </Form.Item>
             
             <Form.Item name="user_value" label="Уровень" rules={[
-                {
-                required: true,
-                message: 'Please input user value!',
-                },
-            ]}>
+                  {
+                  required: true,
+                  message: 'Please input user value!',
+                  },
+              ]}
+            >
               <Radio.Group>
                 <Radio value={1}>Staff</Radio>
                 <Radio value={2}>Manager</Radio>
               </Radio.Group>
             </Form.Item>
 
-            <Form.Item  label="На проценте" name="salary_percent" valuePropName="checked">
-              <Switch />
-            </Form.Item>
+              <Form.Item  label="На проценте" name="salary_percent" valuePropName="checked">
+                <Switch />
+              </Form.Item>
           
               <Form.Item label="На ставке"  name="salary_hour" valuePropName="checked">
                 <Switch />
               </Form.Item>
 
               <Form.Item
-              label="Ставка час"
-              name="hour_tax"
-              rules={[
-                  {
-                  required: false,
-                  },
-              ]}
-              >
-              <Input/>
-            
+                label="Ставка час"
+                name="hour_tax"
+                rules={[
+                    {
+                    required: false,
+                    },
+                ]}
+                >
+                <Input/>
+              
               </Form.Item>
           
 
-             <Form.Item
-            label="Пароль"
-            name="user_password"
-            rules={[
-                {
-                required: true,
-                message: 'Please input password!',
-                },
-            ]}
-            >
-            <Input.Password />
-            </Form.Item>
+              <Form.Item
+                label="Пароль"
+                name="user_password"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Please input password!',
+                    },
+                ]}
+                >
+              <Input.Password />
+              </Form.Item>
           
             <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-                Созадть
-            </Button>
+              <Button type="primary" htmlType="submit">
+                  Созадть
+              </Button>
             </Form.Item>
         </Form>
       </div>
     );
 };
-
-interface User {
-  user_id:string,
-  user_name:string,//имя
-  user_value:number,// 1-user 2-staff 3-admin
-  user_sex:string,// female, male
-  user_phone:string,
-  user_salary_value:{
-    percent_tax:number | boolean,// 50%
-    hour_tax:number,//20-30 лей час
-  },
-  user_salary:string,
-  user_avans:string,
-  user_objects:[
-    {
-      object_id:string,
-      object_name:string,
-    }
-  ],
-  user_inventory:[
-    {
-      inventory_id:string,
-      inventory_name:string,//название инвентаря
-      inventory_quantity:string,//кол-во инвентаря
-    }
-  ],
-  user_materials:[
-    {
-      material_id:string,
-      material_name:string,//название материалов
-      material_quantity:string,//кол-во материалов
-    }
-  ]
-}
-           
--- src/components/molecules/CreateObject/CreateObject.tsx
+++ src/components/molecules/CreateObject/CreateObject.tsx
@@ -24,211 +24,229 @@ interface ObjectForm {
 }
 
 export const CreateObject = () => {
-    const [test,setTest] = React.useState<ObjectForm>();
-    const [value, setValue] = React.useState(0);
-    const layout = {
-        labelCol: {
-          span: 8,
-        },
-        wrapperCol: {
-          span: 16,
-        },
-      };
-
-    const validateMessages = {
-        required: '${label} обязательно!'
-      };
-
-    const onFinish = (values: ObjectForm) => {
-        console.log('Received values of form:',values);
-        setTest(values);
+  const [test,setTest] = React.useState<ObjectForm>();
+  const [value, setValue] = React.useState(0);
+  const layout = {
+      labelCol: {
+        span: 8,
+      },
+      wrapperCol: {
+        span: 16,
+      },
     };
 
-    console.log(test)
-
-    const workerListOptions = [{ value: 'Ваня' }, { value: 'Петя' }, { value: 'Саша' }, { value: 'Дима' }];
-    
-    const workerPriceListOptions = [{ value: 'Установка розетки 1' }, { value: 'Установка розетки 2' }, { value: 'Установка розетки 3' }, { value: 'Установка розетки 4' }];
-
-    const workerRender = (props:any) => {
-        const { label, closable, onClose } = props;
-      
-        return (
-          <Tag color={'cyan'} closable={closable} onClose={onClose} style={{ marginRight: 3, marginTop: 3 }}>
-            {label}
-          </Tag>
-        );
-    }
-
-    const onChange = (event:RadioChangeEvent) => {
-        console.log('radio checked', event.target.value);
-        setValue(event.target.value);
-      };
-
-    return (
-        <div className="create-object-container">
-            <h1>Создать рабочий обьект</h1>
-            <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} autoComplete="off" className="object-form">
-                <Form.Item
-                    name={['object', 'object_name']}
-                    label="Название объекта"
-                    rules={[
-                    {
-                        required: true,
-                    },
-                    ]}
-                >
-                    <Input placeholder="Введите название объекта"/>
-                </Form.Item>
-                <Form.Item name={['object', 'object_description']} label="Описание объекта" rules={[
-                    {
-                        required: true,
-                    },
-                    ]}>
-                    <Input.TextArea placeholder="Введите описание объекта"/>
-                </Form.Item>
-                
-                <Form.Item name={['object', 'object_worker_list']} label="Список работников"
-                    rules={[
-                    {
-                        required: true,
-                    },
-                    ]}>
-                    <Select
-                        placeholder={'Выберите работников'}
-                        mode="multiple"
-                        showArrow
-                        tagRender={workerRender}
-                        style={{ width: '100%' }}
-                        options={workerListOptions}
-                    />
-                </Form.Item>
-                
-                <div className="add-inputs">
-                <Form.List name={['object', 'object_work_list']}>
-                    {(fields, { add, remove }) => (
-                    <>
-                        {fields.map(field => (
-                        <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                            <Form.Item
-                            {...field}
-                            name={[field.name, 'work']}
-                            fieldKey={[field.fieldKey, 'work']}
-                            rules={[{ required: true, message: 'Название работы обязательно!' }]}
-                            >
-                            <Select
-                                placeholder="Выберите"
-                                showArrow
-                                tagRender={workerRender}
-                                style={{ width: '300px' }}
-                                options={workerPriceListOptions}
-                            />
-                            </Form.Item>
-                            <Form.Item
-                            {...field}
-                            name={[field.name, 'quantity']}
-                            fieldKey={[field.fieldKey, 'quantity']}
-                            rules={[{ required: true, message: 'Количество не меньше 0',min: 0,
-                            max: 999,type: 'number' }]}
-                            >
-                            <InputNumber placeholder="кол-во"/>
-                            </Form.Item>
-                            <MinusCircleOutlined onClick={() => remove(field.name)} />
-                        </Space>
-                        ))}
-                        <Form.Item>
-                            <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                                Добавить работу
-                            </Button>
-                        </Form.Item>
-                    </>
-                    )}
-                </Form.List>
-
-                <Form.List name={['object', 'object_material_list']}>
-                    {(fields, { add, remove }) => (
-                    <>
-                        {fields.map(field => (
-                        <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                            <Form.Item
-                            {...field}
-                            name={[field.name, 'material']}
-                            fieldKey={[field.fieldKey, 'material']}
-                            rules={[{ required: true, message: 'Название материалов обязательно!' }]}
-                            >
-                            <Select
-                                placeholder="Выберите"
-                                showArrow
-                                tagRender={workerRender}
-                                style={{ width: '300px' }}
-                                options={workerPriceListOptions}
-                            />
-                            </Form.Item>
-                            <Form.Item
-                            {...field}
-                            name={[field.name, 'quantity']}
-                            fieldKey={[field.fieldKey, 'quantity']}
-                            rules={[{ required: true, message: 'Количество не меньше 0',min: 0,
-                            max: 999,type: 'number' }]}
-                            >
-                            <InputNumber placeholder="кол-во"/>
-                            </Form.Item>
-                            <MinusCircleOutlined onClick={() => remove(field.name)} />
-                        </Space>
-                        ))}
-                        <Form.Item>
-                            <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                                Добавить материалы
-                            </Button>
-                        </Form.Item>
-                    </>
-                    )}
-                </Form.List>
-
-                </div>
-                           
-                <Form.Item
-                    name={['object', 'object_avans']}
-                    label="Аванс"
-                    rules={[
-                    {
-                        type: 'number',
-                        required: true,
-                        min: 0,
-                        max: 9999999
-                    },
-                    ]}
-                >
-                    <InputNumber placeholder="Введите аванс"/>
-                </Form.Item>
-
-                <Form.Item  name={['object', 'object_discount']}
-                    label="Скидка">
-                <Radio.Group onChange={onChange} value={value} name="salary_percent">
-                    <Radio value={0}>0%</Radio>
-                    <Radio value={5}>5%</Radio>
-                    <Radio value={10}>10%</Radio>
-                    <Radio value={15}>15%</Radio>
-                    <Radio value={20}>20%</Radio>
-                </Radio.Group>
-                </Form.Item>
-                
-                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                    <Button type="primary" htmlType="submit" className="object-submit-btn">
-                        Создать
-                    </Button>
-                    
-                    <Button>
-                        Очистить форму
-                    </Button>
-                </Form.Item>
-                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                    <Button>
-                        Сохранить в предосмотр
-                    </Button>
-                </Form.Item>
-            </Form>
-      </div>
-    );
+const validateMessages = {
+  required: '${label} обязательно!'
 };
-           
+
+  const onFinish = (values: ObjectForm) => {
+      console.log('Received values of form:',values);
+      setTest(values);
+  };
+
+  console.log(test)
+
+const workerListOptions = [
+  { value: 'Ваня' },
+  { value: 'Петя' },
+  { value: 'Саша' },
+  { value: 'Дима' }
+];
+
+const workerPriceListOptions = [
+  { value: 'Установка розетки 1' },
+  { value: 'Установка розетки 2' },
+  { value: 'Установка розетки 3' },
+  { value: 'Установка розетки 4' }
+];
+
+
+
+  const workerRender = (props:any) => {
+      const { label, closable, onClose } = props;
+    
+      return (
+        <Tag color={'cyan'} closable={closable} onClose={onClose} style={{ marginRight: 3, marginTop: 3 }}>
+          {label}
+        </Tag>
+      );
+  }
+
+  const onChange = (event:RadioChangeEvent) => {
+      console.log('radio checked', event.target.value);
+      setValue(event.target.value);
+  };
+
+    return (<></>)
+          // <div className="create-object-container">
+          //   <h1>Создать рабочий обьект</h1>
+          //   <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} autoComplete="off" className="object-form">
+          //       <Form.Item
+          //           name={['object', 'object_name']}
+          //           label="Название объекта"
+          //           rules={[
+          //           {
+          //               required: true,
+          //           },
+          //           ]}
+          //       >
+          //         <Form.Item
+          //           {...field}
+          //           name={[field.name, 'work']}
+          //           fieldKey={[field.fieldKey, 'work']}
+          //           rules={[
+          //             {
+          //               required: true,
+          //               message: 'Название работы обязательно!'
+          //             }
+          //           ]}
+          //         >
+          //           <Select
+          //             placeholder="Выберите"
+          //             showArrow
+          //             tagRender={workerRender}
+          //             style={{ width: '300px' }}
+          //             options={workerPriceListOptions}
+          //           />
+          //       </Form.Item>
+                
+          //       <div className="add-inputs">
+          //       <Form.List name={['object', 'object_work_list']}>
+          //           {(fields, { add, remove }) => (
+          //           <>
+          //               {fields.map(field => (
+          //               <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+          //                   <Form.Item
+          //                   {...field}
+          //                   name={[field.name, 'work']}
+          //                   fieldKey={[field.fieldKey, 'work']}
+          //                   rules={[{ required: true, message: 'Название работы обязательно!' }]}
+          //                   >
+          //                   <Select
+          //                       placeholder="Выберите"
+          //                       showArrow
+          //                       tagRender={workerRender}
+          //                       style={{ width: '300px' }}
+          //                       options={workerPriceListOptions}
+          //                   />
+          //                   </Form.Item>
+          //                   <Form.Item
+          //                   {...field}
+          //                   name={[field.name, 'quantity']}
+          //                   fieldKey={[field.fieldKey, 'quantity']}
+          //                   rules={[{ required: true, message: 'Количество не меньше 0',min: 0,
+          //                   max: 999,type: 'number' }]}
+          //                   >
+          //                   <InputNumber placeholder="кол-во"/>
+          //                   </Form.Item>
+          //                   <MinusCircleOutlined onClick={() => remove(field.name)} />
+          //               </Space>
+          //               ))}
+          //               <Form.Item>
+          //                   <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+          //                       Добавить работу
+          //                   </Button>
+          //               </Form.Item>
+          //           </>
+          //           )}
+          //       </Form.List>
+
+          //       <Form.List name={['object', 'object_material_list']}>
+          //           {(fields, { add, remove }) => (
+          //           <>
+          //               {fields.map(field => (
+          //               <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+          //                   <Form.Item
+          //                   {...field}
+          //                   name={[field.name, 'material']}
+          //                   fieldKey={[field.fieldKey, 'material']}
+          //                   rules={[{ required: true, message: 'Название материалов обязательно!' }]}
+          //                   >
+          //                   <Select
+          //                       placeholder="Выберите"
+          //                       showArrow
+          //                       tagRender={workerRender}
+          //                       style={{ width: '300px' }}
+          //                       options={workerPriceListOptions}
+          //                   />
+          //                   </Form.Item>
+          //                   <Form.Item
+          //                   {...field}
+          //                   name={[field.name, 'quantity']}
+          //                   fieldKey={[field.fieldKey, 'quantity']}
+          //                   rules={[{ required: true, message: 'Количество не меньше 0',min: 0,
+          //                   max: 999,type: 'number' }]}
+          //                   >
+          //                   <InputNumber placeholder="кол-во"/>
+          //                   </Form.Item>
+          //                   <MinusCircleOutlined onClick={() => remove(field.name)} />
+          //               </Space>
+          //               ))}
+          //               <Form.Item>
+          //                   <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+          //                       Добавить материалы
+          //                   </Button>
+          //               </Form.Item>
+          //           </>
+          //           )}
+          //       </Form.List>
+
+          //       </div>
+                           
+          //       <Form.Item
+          //           name={['object', 'object_avans']}
+          //           label="Аванс"
+          //           rules={[
+          //             {
+          //               required: true,
+          //               message: 'Количество не меньше 0',
+          //               min: 0,
+          //               max: 999,
+          //               type: 'number'
+          //             }
+          //           ]}
+          //         >
+          //           <InputNumber placeholder="кол-во" />
+          //         </Form.Item>
+          //         {/* <MinusCircleOutlined onClick={() => remove(field.name)} /> */}
+               
+            
+          //       <Form.Item>
+          //         <Button
+          //           type="dashed"
+          //           // onClick={() => add()}
+          //           block
+          //           icon={<PlusOutlined />}
+          //         >
+          //             <InputNumber placeholder="Введите аванс"/>
+          //         </Button>    
+          //       </Form.Item>
+
+          //       <Form.Item  name={['object', 'object_discount']}
+          //           label="Скидка">
+          //       <Radio.Group onChange={onChange} value={value} name="salary_percent">
+          //           <Radio value={0}>0%</Radio>
+          //           <Radio value={5}>5%</Radio>
+          //           <Radio value={10}>10%</Radio>
+          //           <Radio value={15}>15%</Radio>
+          //           <Radio value={20}>20%</Radio>
+          //       </Radio.Group>
+          //       </Form.Item>
+                
+          //       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          //           <Button type="primary" htmlType="submit" className="object-submit-btn">
+          //               Создать
+          //           </Button>
+                    
+          //           <Button>
+          //               Очистить форму
+          //           </Button>
+          //       </Form.Item>
+          //       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          //           <Button>
+          //               Сохранить в предосмотр
+          //           </Button>
+          //       </Form.Item>
+          //   </Form>
+          // </div>
+}
